4a6ac22ddfc0410c14d283f50b3da92b7a844f95
tyctw <tyctw.analyze@gmail.com>
2026-06-10 02:26:08 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 
 export default function App() {
   return (
-    <BrowserRouter basename="/form">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/admin" element={<Admin />} />
+    </Routes>
   );
 }
