36a2be790e0b9874cab14b62fcf89060a55f2f22
tyctw <tyctw.analyze@gmail.com>
2026-06-10 12:58:33 +0800

    Switch from BrowserRouter to HashRouter
    Replaced BrowserRouter with HashRouter and removed basename.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // 修改這裡
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/form">
+    <HashRouter> {/* 修改這裡，並移除 basename */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
